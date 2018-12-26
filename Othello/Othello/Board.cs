83936730a9030995573a398e21ce87c0c0e81b70
Damian Petroff <damian.petroff@he-arc.ch>
2018-12-26 12:03:18 +0100

    Same as it was before, but a bit cleaner. All game logic must be implemented in Board.cs

---
+++ Othello/Othello/Board.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Othello
+{
+    class OthelloBoard
+    {
+        private int[,] values;
+        private int width;
+        private int height;
+
+        public int[,] Values { get => values; }
+        public int Width { get => width;}
+        public int Height { get => height;}
+
+        //enum CellState { BLACK=-1, WHITE=1, EMPTY=0};
+        public OthelloBoard(int width, int height)
+        {
+            this.height = height;
+            this.width = width;
+            values = new int[height, width];
+            MakeInitialBoard();
+        }
+
+        private void MakeInitialBoard()
+        {
+            for (var y = 0; y < width; y++)
+                for (var x = 0; x < height; x++)
+                    values[y,x] = 0;
+            values[height / 2 - 1, width / 2 - 1] = 1;
+            values[height / 2 , width / 2 - 1] = -1;
+            values[height / 2 - 1, width / 2] = -1;
+            values[height / 2, width / 2] = 1;
+        }
+
+        public void Play(int x, int y, int value)
+        {
+            values[y, x] = value;
+        }
+
+        public void DisplayBoard()
+        {
+            for (int y = 0; y < Height; y++)
+            {
+                for (int x = 0; x < Width; x++)
+                {
+                    Console.Write($"{values[y,x]}");
+                }
+                Console.Write(Environment.NewLine + Environment.NewLine);
+            }
+            Console.ReadLine();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder debug = new StringBuilder();
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Console.Write($"[{values[y,x]}]");
+                    debug.Append($"[{values[y,x]}]");
+                }
+                debug.Append("\n");
+            }
+            Console.WriteLine(debug.ToString());
+            return debug.ToString();
+        }
+    }
+}
