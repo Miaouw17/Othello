cca1f476233e03b12ab5322d46b1eca80e98b99f
quentin.michel <quentin.michel@he-arc.ch>
2019-01-09 08:18:58 +0100

    just trying something with multiple page and a frame on mainwindow

---
+++ Othello/Othello/Menu.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Othello
+{
+    /// <summary>
+    /// Logique d'interaction pour Menu.xaml
+    /// </summary>
+    public partial class Menu : Page
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void NewGame_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LoadGame_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
